6640e1677006fc0e36c0001a7c30057dfea4766e
CrushDavis <71688475+CrushDavis@users.noreply.github.com>
2025-06-22 10:42:36 +0900

    Add files via upload

---
+++ app/src/main/java/model/response/ResponseUser.kt
@@ -0,0 +1,12 @@
+package model.response
+import java.time.LocalDateTime
+
+data class ResponseUser (
+    val id: Long,
+    val email: String,
+    val password: String,
+    val username: String,
+    val phoneNumber: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+)
